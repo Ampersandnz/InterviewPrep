c8a7277c2bba2d624aae1988c3ee68b320001638
Michael Lo <nz.ampersand@gmail.com>
2016-03-01 20:45:25 +1300

    Completed question 1.1

--- 1 - Arrays & Strings/1.1/1.1.py
+++ 1 - Arrays & Strings/1.1/1.1.py
@@ -1,1 +1,35 @@
 __author__ = 'Michael'
+
+# Implement an algorithm to determine if a string has all unique characters.
+# What if you cannot use additional data structures?
+
+not_unique = "i am not a unique string"
+unique = "but_i.am "
+
+
+def main():
+    print(is_unique(unique))
+    print(is_unique(not_unique))
+
+    print(is_unique_no_structs(unique))
+    print(is_unique_no_structs(not_unique))
+
+
+def is_unique(to_check):
+    hashmap = {}
+
+    for char in to_check:
+        if char in hashmap:
+            return False
+        hashmap[char] = char
+    return True
+
+
+def is_unique_no_structs(to_check):
+    for char in to_check:
+        if to_check.count(char) > 1:
+            return False
+    return True
+
+if __name__ == '__main__':
+    main()

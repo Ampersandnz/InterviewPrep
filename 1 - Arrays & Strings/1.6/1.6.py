f9003fed9f63f16510ace03f40c7e5e4a2cec7de
Michael Lo <nz.ampersand@gmail.com>
2016-03-04 20:09:00 +1300

    Added base files for question 1.6

---
+++ 1 - Arrays & Strings/1.6/1.6.py
@@ -0,0 +1,19 @@
+__author__ = 'Michael'
+
+# Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
+# write a method to rotate the image by 90 degrees.
+# Can you do this in place?
+
+matrix = []
+
+
+def main():
+    print(rotate(matrix))
+
+
+def rotate(image_matrix):
+    return image_matrix
+
+
+if __name__ == '__main__':
+    main()

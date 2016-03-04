c65ea1857174a6038e82a30f70cee600b4f4c5af
Michael Lo <nz.ampersand@gmail.com>
2016-03-04 20:14:36 +1300

    Added base files for question 1.7

---
+++ 1 - Arrays & Strings/1.7/1.7.py
@@ -0,0 +1,19 @@
+__author__ = 'Michael'
+
+# Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
+
+matrix = []
+m = 0
+n = 0
+
+
+def main():
+    print(zero(matrix))
+
+
+def zero(the_matrix):
+    return the_matrix
+
+
+if __name__ == '__main__':
+    main()

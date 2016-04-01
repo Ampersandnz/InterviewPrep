9ae9ff89dd725b1bfc2c1a75bec967801a447bc1
Michael <nz.ampersand@gmail.com>
2016-04-01 15:07:53 +1300

    Implemented Fibonacci number program, with optional caching.

--- 0 - Other, Interest/Caching/src/caching/Caching.java
+++ 0 - Other, Interest/Caching/src/caching/Caching.java
@@ -1,10 +1,40 @@
 package caching;
 
+import java.util.Arrays;
+
+/* Author: Michael */
+
 public class Caching {
+	
+	private static boolean __CACHING_ENABLED = true;
+	//private static List<Integer> __CACHE = new ArrayList<Integer>();
+	private static int[] __CACHE = new int[100];
+	
+	public static void main(String[] args) {		
+		Arrays.fill(__CACHE, -1);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(fibonacci(25));
+	}
+	
+	public static int fibonacci(int n) {
+		int fib = 0;
+		
+		if (__CACHING_ENABLED) {
+			if (__CACHE[n] != -1) {
+				return __CACHE[n];
+			}
+		}
+		
+		if (n == 0) {
+		} else if (n == 1) {
+			fib = 1;
+		} else {
+			fib = fibonacci(n - 1) + fibonacci(n - 2);
+		}	
+		
+		__CACHE[n] = fib;
+		
+		return fib;
 	}
 
 }

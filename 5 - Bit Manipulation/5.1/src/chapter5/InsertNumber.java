821b0ba081d0ae697163be39ce78d8ca7f456efd
Michael <nz.ampersand@gmail.com>
2016-05-26 14:55:07 +1200

    Completed question 5.1

--- 5 - Bit Manipulation/5.1/src/chapter5/InsertNumber.java
+++ 5 - Bit Manipulation/5.1/src/chapter5/InsertNumber.java
@@ -13,29 +13,36 @@ public class InsertNumber {
 
 	public static void main(String[] args) {
 		int M = 0b1_0011; // 19
-		int N = 0b100_0000_0000; // 1024
+		int N = 0b100_0000_1111; // 1024
 		int i = 2;
 		int j = 6;
 		
-		insertBinary(M, N, i, j);
-		System.out.println(getBit(N, i));
-		printBinary(setBit(N, i));
-		printBinary(clearBit(N, i));
+		printBinary(insertBinary(M, N, i, j));
 	}
 
+	// There's probably a way to make a mask and do this more efficiently all in one go, but this will do for now
 	private static int insertBinary(int M, int N, int i, int j) {
-		System.out.println("About to insert " + M + " into " + N + " at bits " + i + " through " + j);
-		
-		// TODO: Shift bits N[i+] left by numBits(M)
-		// TODO: Set bits j through i to the bits of M
-		for (int x = j; x > 0; x--) {
-			if (x >= i) {
-				//copy bit from M
-			} else {
-				//copy bit from N
-			}
+		// Sanity checks
+		if (i < 1) {
+			System.out.println("Invalid input! i cannot be less than 1");
+			return -1;
+		} else if (j > numBits(N)) {
+			System.out.println("Invalid input! j cannot be larger than the length of N in binary (" + numBits(N) + ")");
+			return -1;
+		} else if (((j - i) + 1) != numBits(M)) {
+			System.out.println("Invalid input! i - j must equal the length of M in binary (" + numBits(M) + ")");
+			return -1;
 		}
-		return 0;
+		
+		int y = numBits(M) - 1;
+		
+		// Replace bits i through j
+		for (int x = j; x >= 0; x--) {
+			N = copyBit(N, x, getBit(M, y));
+			y -= 1;
+		}
+		
+		return N;
 	}
 	
 	private static int getBit(int num, int i) {
@@ -49,6 +56,7 @@ public class InsertNumber {
 		return isSet ? 1 : 0;
 	}
 	
+	// I'm allowing i > number of bits in num - just extends the length of the number
 	private static int setBit(int num, int i) {
 		// Makes a mask of pattern 1[i 0s]
 		int mask = (1 << i);
@@ -69,7 +77,15 @@ public class InsertNumber {
 		return result;
 	}
 	
+	private static int copyBit(int num, int i, int bit) {	
+		return (bit == 1) ? setBit(num, i) : clearBit(num, i);
+	}
+	
 	private static void printBinary(int num) {
 		System.out.println(Integer.toBinaryString(num));
+	}
+	
+	private static int numBits(int num) {
+		return (int) (Math.floor(Math.log(num) / Math.log(2)) + 1);
 	}
 }

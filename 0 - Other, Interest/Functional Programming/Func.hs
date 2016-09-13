4f03cdf6833822a4d3e273d17a96f9831b477f61
Michael <nz.ampersand@gmail.com>
2016-09-13 12:36:02 +1200

    Continued through tutorial course.

--- 0 - Other, Interest/Functional Programming/Func.hs
+++ 0 - Other, Interest/Functional Programming/Func.hs
@@ -18,3 +18,17 @@ doubleOddSmallListValues y = [double x | x <- y, x < 5, odd x]
 
 -- Returns triples containing the lengths of the sides of the right-angled triangles that have sides 10 units or shorter, and a perimeter of 24
 smallRightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+
+-- Pattern matching as a function definition. Note that x is just a label, any non-literal value will catch all other inputs
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+-- First look at Haskell recursion
+factorial :: (Integral a) => a -> a  
+factorial 0 = 1  
+factorial n = n * factorial (n - 1)
+
+--Pattern matching on tuple input. Note that the pattern IS a catch-all, as the function definition forces an input of two tuples.
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)  
